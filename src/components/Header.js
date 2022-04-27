295f62eee3cefe1268e5fc505ed045af6da815a2
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-04-27 20:34:03 +0900

    Main files set up

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header() {
+    
+    console.log("I'm in the Header")
+    return (
+        <header>
+            <h2>Nook Phone</h2>
+        </header>
+    )
+}
