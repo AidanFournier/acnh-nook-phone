a34bb4d87cfbf8bcf2ddbe2c986a8cf9ac5cfc40
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-05-26 12:28:19 +0900

    Removed Main.js, passed styling to App.js main div

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react"
-import Main from "./components/Main"
+// import Card from "./components/Card"
 
 export default function App() {
   return (
-    <div>
-      <Main />
+    <div className="main">
     </div>
   );
 }
