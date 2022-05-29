03a6078121d1c0aaaf2f1f017db91c3b50765e91
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-05-30 00:57:25 +0900

    Added location pin to nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
 import React from "react"
+import Location from "../images/location.png"
+
 
 export default function Header() {
+    
+    
+    
     return (
         <div className="header">
             <nav>
                 <div className="header--nav-service"></div>
                 <div className="header--nav-time">1:00 PM</div>
-                <div className="header--nav-location"></div>
+
+                    <img src= {Location} alt="Location pin" className="header--nav-location" />
+               
             </nav>
             <h1>Villager Wiki</h1>
         </div>
