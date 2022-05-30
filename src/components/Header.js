12d5e2a05f90937c182402c74478d421918e3869
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-05-30 23:17:56 +0900

    Navbar visually complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Location from "../images/location.png"
+import Service from "../images/service.png"
 
 
 export default function Header() {
@@ -9,11 +10,9 @@ export default function Header() {
     return (
         <div className="header">
             <nav>
-                <div className="header--nav-service"></div>
+                <img src= {Service} alt="Location pin" className="header--nav-service" />
                 <div className="header--nav-time">1:00 PM</div>
-
-                    <img src= {Location} alt="Location pin" className="header--nav-location" />
-               
+                <img src= {Location} alt="Location pin" className="header--nav-location" />
             </nav>
             <h1>Villager Wiki</h1>
         </div>
