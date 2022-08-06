097496838b8243de20749e2ca7578b43c697fce2
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-08-06 19:50:43 +0900

    Created 401 page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,7 +1,15 @@
 import React from "react"
 
+import SheepishIcon from "../images/sheepish-icon.png"
+import ACNHLogo from "../images/animal-crossing-logo.png"
+
 export default function Error() {
     return (
-        <div>Oopsie</div>
+        <div className="error-page">
+            <img src={SheepishIcon} className="character-icon" alt="Sheepish icon"/>
+            <h2>Uh oh!</h2>
+            <p>Villager not found. Check your spelling and try again!</p>
+            <img src= {ACNHLogo} alt="Animal Crossing official logo" className="acnh-logo" />
+        </div>
     )
 }
