4f2ddda5caa5c7f2ba2669ce747effd782974ca5
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-08-16 23:14:34 +0900

    Successfully switched stylesheet from css to scss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './fonts/FinkHeavy.ttf'; 
-import './style.css';
+import './stylesheets/application.scss';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById("root"));
